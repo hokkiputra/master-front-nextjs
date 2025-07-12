0d3102e7a5e8ece30b91236145d9835246da72fc
hokkiputra <handikabari@gmail.com>
2025-07-12 21:11:20 +0700

    Update API URL to production

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 const axios = Axios.create({
   // baseURL: 'http://127.0.0.1:8000/api',
-  baseURL: 'http://absensi-api.test/api',
+  baseURL: 'https://sahil-api.perintisilmu.sch.id/api',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
