695500d385b5a009adc335793b5865f2d6c46d22
hokkiputra <handikabari@gmail.com>
2026-02-01 11:43:38 +0700

    fix: use env-based api url for axios

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,8 +2,9 @@ import Axios from 'axios'
 
 const axios = Axios.create({
   // baseURL: 'http://127.0.0.1:8000/api',
-  //baseURL: 'https://sahil-api.perintisilmu.sch.id/api',
-   baseURL: 'http://sahil-api.test/api',
+  // baseURL: 'https://sahil-api.perintisilmu.sch.id/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  // baseURL: 'http://sahil-api.test/api',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
